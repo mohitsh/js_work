9dcde6a3247eaf25ec44a07e93cff34407e14648
mohitsh <mohitsh114@gmail.com>
2015-08-21 13:00:45 +0530

    falsy_bouncer.js

---
+++ falsy_bouncer.js
@@ -0,0 +1,16 @@
+
+
+function bouncer(arr) {
+  // Don't show a false ID to this bouncer.
+  return arr.filter(ans);
+ 
+}
+function ans(x){
+  
+  if (x !== false || x !== null || x !== 0 || x !== '' || x !== undefined || x !== NaN){
+    return x;
+  }
+}
+
+bouncer([7, 'ate', '', false, 9]);
+
